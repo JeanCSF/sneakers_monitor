235abb5f610e590e41ba5d617d01992432826b8b
JeanCSF <jean.csf.17@gmail.com>
2023-12-19 16:06:00 -0300

    new store add (sunika)

--- backend/mainCluster.js
+++ backend/mainCluster.js
@@ -4,15 +4,28 @@ const { getLinks, processLink } = require('./src/utils/utils');
 const allResultsSet = new Set();
 
 const urls = [
-    'https://www.artwalk.com.br/',
-    'https://www.correderua.com.br/',
-    'https://www.lojavirus.com.br/',
-    'https://www.gdlp.com.br/',
+    'https://www.sunika.com.br/',
+    // 'https://www.artwalk.com.br/',
+    // 'https://www.correderua.com.br/',
+    // 'https://www.lojavirus.com.br/',
+    // 'https://www.gdlp.com.br/',
     // 'https://youridstore.com.br/',
 ];
 
 const storesObj = {
-    'correderua': {
+    sunika: {
+        name: 'Sunika',
+        baseUrl: 'https://www.sunika.com.br/',
+        selectors: {
+            links: 'li.purchasable',
+            productReference: '',
+            img: '.big-list',
+            sneakerName: '.information div .name',
+            price: '.priceContainer > strong > span',
+            availableSizes: '.options > label:not(.unavailable) > span > b',
+        }
+    },
+    correderua: {
         name: 'CDR',
         baseUrl: 'https://www.correderua.com.br/',
         selectors: {
@@ -22,10 +35,10 @@ const storesObj = {
             sneakerName: '.nome-produto.titulo.cor-secundaria',
             price: '.preco-produto.destaque-preco strong[data-sell-price]',
             availableSizes: 'a.atributo-item:not(.indisponivel)',
-            pagination: '',
+            pagination: '.pagination ul li a',
         },
     },
-    'artwalk': {
+    artwalk: {
         name: 'Artwalk',
         baseUrl: 'https://www.artwalk.com.br/',
         selectors: {
@@ -38,7 +51,7 @@ const storesObj = {
             pagination: '',
         },
     },
-    'gdlp': {
+    gdlp: {
         name: 'GDLP',
         baseUrl: 'https://www.gdlp.com.br/',
         selectors: {
@@ -51,7 +64,7 @@ const storesObj = {
             pagination: '.amount--has-pages',
         },
     },
-    'lojavirus': {
+    lojavirus: {
         name: 'LojaVirus',
         baseUrl: 'https://www.lojavirus.com.br/',
         selectors: {
@@ -69,28 +82,29 @@ const storesObj = {
 
 const searchFor = [
     'converse',
-    'air force',
-    'adidas superstar',
-    'air max',
-    'air jordan',
-    'adidas forum',
-    'adidas samba',
-    'adidas gazelle',
-    'adidas campus',
-    'adidas ADI2000',
-    'puma suede',
-    'puma basket',
-    'puma 180',
-    'puma slipstream',
-    'reebok classic',
-    'reebok club c',
-    'vans old skool',
-    'vans authentic',
-    'vans sk8',
-    'vans era',
-    'vans ultrarange',
-    'asics gel',
-    'fila corda'
+    // 'fila',
+    // 'air force',
+    // 'adidas superstar',
+    // 'air max',
+    // 'air jordan',
+    // 'adidas forum',
+    // 'adidas samba',
+    // 'adidas gazelle',
+    // 'adidas campus',
+    // 'adidas ADI2000',
+    // 'puma suede',
+    // 'puma basket',
+    // 'puma 180',
+    // 'puma slipstream',
+    // 'reebok classic',
+    // 'reebok club c',
+    // 'vans old skool',
+    // 'vans authentic',
+    // 'vans sk8',
+    // 'vans era',
+    // 'vans ultrarange',
+    // 'asics gel',
+    // 'fila corda'
 ];
 
 async function mainCluster() {
@@ -129,8 +143,9 @@ async function mainCluster() {
         try {
             const storeObj = storesObj[url.replace(/.*?\/\/(?:www\.)?(.*?)\.com.*/, '$1')];
             const links = await getLinks(page, url, storeObj, term);
-
-            links.forEach(link => allResultsSet.add(link));
+            if (links !== undefined) {
+                links.forEach(link => allResultsSet.add(link));
+            }
         } catch (error) {
             console.error(`Error processing ${url} with term ${term}:`, error.message);
             console.error(error.stack);
--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -3,18 +3,19 @@ const { Decimal128 } = require("mongodb");
 const randomUserAgent = require('random-useragent');
 const ExcelJS = require("exceljs");
 const path = require("path");
-const onTest = false;
+const onTest = true;
 const workbook = new ExcelJS.Workbook();
 const worksheet = workbook.addWorksheet("Test Data");
 worksheet.addRow(["Store", "Product Reference", "Sneaker Name", "Current Price", "Discount Price", "Available Sizes", "Date", "Link", "Image"]);
 
-async function scrollToEnd(page) {
-    await page.evaluate(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-    });
-};
 
 async function scrollPage(page) {
+    async function scrollToEnd(page) {
+        await page.evaluate(() => {
+            window.scrollTo(0, document.body.scrollHeight);
+        });
+    };
+
     let previousHeight = 0;
     while (true) {
         await scrollToEnd(page);
@@ -37,20 +38,25 @@ function arraysEqual(arr1, arr2) {
     return true;
 }
 
-function strToDecimal(str) {
-    const cleanedStr = str.replace(/\./g, '').replace(',', '.');
-    return parseFloat(cleanedStr);
+function parseDecimal(str) {
+    const cleanedStr = str.replace(/[^0-9.,]/g, '');
+    const dotDecimalStr = cleanedStr.replace(/\./g, '').replace(',', '.');
+    return new Decimal128(dotDecimalStr);
 }
+
 
 async function updateOrCreateSneaker(sneakerObj) {
     const { productReference, store } = sneakerObj;
     try {
         if (onTest) {
-
-            worksheet.addRow([sneakerObj.store, sneakerObj.productReference, sneakerObj.sneakerName, sneakerObj.currentPrice, sneakerObj.discountPrice, sneakerObj.availableSizes.join(', '), new Date().toLocaleString(), sneakerObj.srcLink, sneakerObj.img]);
-            const filePath = path.join(__dirname, "test_data.xlsx");
-            await workbook.xlsx.writeFile(filePath);
-            console.log(`Sneaker ${sneakerObj.sneakerName} added to test data: ${filePath}`);
+            if (sneakerObj.availableSizes.length >= 1) {
+                worksheet.addRow([sneakerObj.store, sneakerObj.productReference, sneakerObj.sneakerName, sneakerObj.currentPrice, sneakerObj.discountPrice, sneakerObj.availableSizes.join(', '), new Date().toLocaleString(), sneakerObj.srcLink, sneakerObj.img]);
+                const filePath = path.join(__dirname, "test_data.xlsx");
+                await workbook.xlsx.writeFile(filePath);
+                console.log(`Sneaker ${sneakerObj.sneakerName} added to test data: ${filePath}`);
+            } else {
+                console.log(`Sneaker ${sneakerObj.sneakerName} has no available sizes.`);
+            }
         } else {
             const existingSneaker = await SneakerModel.findOne({ store, productReference });
             if (existingSneaker) {
@@ -84,6 +90,8 @@ function createSearchUrl(url, term) {
         return `${url}${encodeURIComponent(term).toLowerCase()}?O=OrderByPriceASC&PS=24`;
     } else if (url.includes("lojavirus")) {
         return `${url}busca?busca=${term.replace(/\s+/g, "-").toLowerCase()}`;
+    } else if (url.includes("sunika")) {
+        return `${url}pesquisa?t=${term.replace(/\s+/g, "+").toLowerCase()}`;
     } else {
         console.error("Invalid URL:", url);
         return url;
@@ -105,13 +113,16 @@ async function interceptRequests(page) {
         }
     });
     page.on('dialog', async (dialog) => {
-        await dialog.accept();
+        if (dialog) {
+            await dialog.accept();
+        }
     });
 }
 
 async function getNumOfPages(page, storeName, storeSelectors) {
     try {
         switch (storeName) {
+            case "CDR":
             case "LojaVirus":
                 return await page.evaluate((selectors) => {
                     const links = document.querySelectorAll(selectors.pagination);
@@ -135,32 +146,54 @@ async function getNumOfPages(page, storeName, storeSelectors) {
     }
 }
 
+async function iteratePaginationLinks(page, pageNumbers, newUrl, storeObj, term) {
+    try {
+        const newLinks = [];
+        for (let i = 2; i <= pageNumbers; i++) {
+            switch (storeObj.name) {
+                case "CDR":
+                case "LojaVirus":
+                    await page.goto(`${newUrl}?pagina=${i}`, { waitUntil: 'domcontentloaded' });
+                    break;
+
+                case "GDLP":
+                    await page.goto(`${storeObj.baseUrl}search/page/${i}?q=${term.replace(/\s+/g, "+").toLowerCase()}`, { waitUntil: 'load' });
+                    storeObj.name === "GDLP" ? await page.waitForTimeout(3000) : null;
+                    break;
+
+                default:
+                    break;
+            }
+            const links = await page.$$eval(storeObj.selectors.links, (containers) => {
+                return containers.map(container => container.querySelector("a").href);
+            });
+
+            newLinks.push(...links);
+        }
+        return newLinks;
+    } catch (error) {
+        console.error("Error iterating links from pagination:", error);
+        console.error(error.stack);
+    }
+}
+
 async function getLinks(page, url, storeObj, term) {
     try {
         await page.setUserAgent(randomUserAgent.getRandom());
         await interceptRequests(page);
         await page.goto(createSearchUrl(url, term), { waitUntil: 'domcontentloaded' });
-
+        const newUrl = await page.url();
         await scrollPage(page);
 
         const links = await page.$$eval(storeObj.selectors.links, (containers) => {
             return containers.map(container => container.querySelector("a").href);
         });
+
         const pageNumbers = await getNumOfPages(page, storeObj.name, storeObj.selectors);
 
         if (pageNumbers !== null && pageNumbers > 1) {
-            for (let i = 2; i <= pageNumbers; i++) {
-                if (storeObj.name === "LojaVirus") {
-                    await page.goto(`${url}?pagina=${i}`, { waitUntil: 'domcontentloaded' });
-                } else {
-                    await page.goto(`${url}search/page/${i}?q=${term.replace(/\s+/g, "+").toLowerCase()}`, { waitUntil: 'load' });
-                    storeObj.name === "GDLP" ? await page.waitForTimeout(3000) : null;
-                }
-                const newLinks = await page.$$eval(storeObj.selectors.links, (containers) => {
-                    return containers.map(container => container.querySelector("a").href);
-                });
-                links.push(...newLinks);
-            }
+            const newLinks = await iteratePaginationLinks(page, pageNumbers, newUrl, storeObj, term);
+            links.push(...newLinks);
         }
 
         if (links.length !== 0) {
@@ -188,7 +221,6 @@ async function processLink(page, link, storeObj) {
         await Promise.all([
             page.goto(link, { waitUntil: storeObj.name === "GDLP" ? "load" : "domcontentloaded" }),
             page.waitForSelector(storeObj.selectors.sneakerName),
-            page.waitForSelector(storeObj.selectors.productReference),
             page.waitForSelector(storeObj.selectors.img),
             page.waitForSelector(storeObj.selectors.price),
         ]);
@@ -197,7 +229,8 @@ async function processLink(page, link, storeObj) {
         storeObj.name === "GDLP" ? await page.waitForTimeout(3000) : null;
         const availableSizes = await getAvailableSizes(page, storeObj);
         const sneakerName = await getSneakerName(page, storeObj);
-        const productReference = await getProductReference(page, storeObj);
+        const productReference = await getProductReference(page, storeObj, link);
+        console.log(`productReference: ${productReference}`);
         const img = await getImg(page, storeObj);
         const price = await getPrice(page, storeObj);
         const discountPrice = await getDiscountPrice(page, storeObj);
@@ -240,7 +273,7 @@ async function getAvailableSizes(page, storeObj) {
                 .filter(el => !el.classList.contains('item_unavailable') && !el.classList.contains('disabled'))
                 .map((el) => {
                     const sizeText = el.innerText.trim();
-                    const numericSize = parseFloat(sizeText.replace(/\D/g, ''));
+                    const numericSize = parseFloat(sizeText.replace(',', '.')); // Substitui vírgula por ponto
                     return !isNaN(numericSize) ? numericSize : null;
                 })
                 .filter((size) => size !== null);
@@ -254,6 +287,7 @@ async function getAvailableSizes(page, storeObj) {
     }
 }
 
+
 async function getSneakerName(page, storeObj) {
     try {
         const sneakerName = await page.$eval(storeObj.selectors.sneakerName, (el) => el.innerText.trim());
@@ -264,15 +298,22 @@ async function getSneakerName(page, storeObj) {
     }
 }
 
-async function getProductReference(page, storeObj) {
+async function getProductReference(page, storeObj, link) {
     try {
-        const productReference = storeObj.name.toLowerCase().includes("lojavirus")
-            ? await page.$eval(storeObj.selectors.productReference, (el) => {
-                const match = el.innerText.match(/\b([A-Za-z0-9-]+)\b$/);
-                return match ? match[1] : el.innerText;
-            })
-            : await page.$eval(storeObj.selectors.productReference, (el) => el.innerText);
-        return productReference;
+        if (storeObj.name.toLowerCase().includes("sunika")) {
+            const match = link.match(/\/([^/]+)-p(\d+)$/);
+            if (match) {
+                return match[2];
+            }
+        } else {
+            const productReference = storeObj.name.toLowerCase().includes("lojavirus")
+                ? await page.$eval(storeObj.selectors.productReference, (el) => {
+                    const match = el.innerText.match(/\b([A-Za-z0-9-]+)\b$/);
+                    return match ? match[1] : el.innerText;
+                })
+                : await page.$eval(storeObj.selectors.productReference, (el) => el.innerText);
+            return productReference;
+        }
     } catch (error) {
         console.error("Error getting product reference:", error);
         console.error(error.stack);
@@ -299,9 +340,9 @@ async function getPrice(page, storeObj) {
             if (match) {
                 return match[1];
             }
-            return strToDecimal(sellPriceAttribute);
+            return sellPriceAttribute.replace('.', ',');
         });
-        return strToDecimal(price);
+        return parseDecimal(price);
     } catch (error) {
         console.error("Error getting price:", error);
         console.error(error.stack);
@@ -320,7 +361,7 @@ async function getDiscountPrice(page, storeObj) {
                 return null;
             })
             : null;
-        return discountPrice ? strToDecimal(discountPrice) : discountPrice;
+        return discountPrice ? parseDecimal(discountPrice) : discountPrice;
     } catch (error) {
         console.error("Error getting discount price:", error);
         console.error(error.stack);
